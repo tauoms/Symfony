64792914a0c9f8c636c2b235a44cf3aee1523581
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-04-24 12:01:03 +0300

    Create PalindromeController

---
+++ palindrome_checker/src/Controller/PalindromeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class PalindromeController extends AbstractController {
+
+    #[Route('/', name: "palindrome_checker")]
+    public function index(Request $request) : Response
+    { 
+        $inputStr = $request->query->get('inputStr');
+        $isPalindrome = false;
+
+		if (!empty($inputStr)) { 
+			$isPalindrome = true; 
+			}
+			
+			return $this->render('palindrome_checker/index.html.twig', [
+				'inputStr' => $inputStr,
+				'isPalindrome' => $isPalindrome, 
+			]); 
+		}
+	}
