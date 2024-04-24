0d098249f6d9df29266567ed9f5a05bdebac64a4
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-04-24 12:49:03 +0300

    Add switch statement for calculation

---
+++ symfony_calculator/src/Controller/CalculatorController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class CalculatorController extends AbstractController {
+
+    #[Route('/', name: "calculator")]
+    public function index(Request $request) : Response
+    { 
+        $value1 = $request->query->get('value1');
+        $value2 = $request->query->get('value2');
+        $operation = $request->query->get('operation');
+        $solution = '';
+
+		if (!empty($value1) && !empty($value2)) { 
+			switch ($operation) {
+				case 'addition':
+					$solution = $value1 + $value2;
+					break;
+				case 'subtraction':
+					$solution = $value1 - $value2;
+					break;
+				case 'multiplication':
+					$solution = $value1 * $value2;
+					break;
+				case 'division':
+					$solution = $value1 / $value2;
+					break;
+				}
+			}
+			
+			return $this->render('calculator/index.html.twig', [
+				'solution' => $solution,
+			]); 
+		}
+	}
