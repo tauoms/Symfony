0d3b69dd915198434ad0e4a9969539252ec7c8f5
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-04-24 10:43:31 +0300

    Install symfony/twig-bundle, create Home page

---
+++ link_pages/src/Controller/HomeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class HomeController extends AbstractController {
+
+    #[Route('/', name: "home")]
+    public function homePage() : Response
+    { 
+        $homeData = [
+			"Home data 1" => "some data",
+			"Home data 2" => "some more data",
+			"Home data 3" => "some even more data",
+		];
+
+		
+			
+			return $this->render('link_pages/index.html.twig', [
+				'homeData' => $homeData, 
+			]); 
+		}
+	}
