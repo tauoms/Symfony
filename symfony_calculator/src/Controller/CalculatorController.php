456843322d5500d7e88c3aa499f20192b23d8b34
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-04-25 10:19:37 +0300

    Add example of using LoggerInterface in method

--- symfony_calculator/src/Controller/CalculatorController.php
+++ symfony_calculator/src/Controller/CalculatorController.php
@@ -6,12 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Psr\Log\LoggerInterface;
 
 class CalculatorController extends AbstractController {
 
     #[Route('/', name: "calculator")]
-    public function index(Request $request) : Response
+    public function index(Request $request, LoggerInterface $logger) : Response
     { 
+		$logger->info('Using Logger service.');
+
         $value1 = $request->query->get('value1');
         $value2 = $request->query->get('value2');
         $operation = $request->query->get('operation');
