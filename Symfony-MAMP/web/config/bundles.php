1ad4cc58f992e32c3103d8eb166dedd06da355bd
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-05-01 17:06:36 +0300

    Remove Symfony-MAMP (moved to its own repo)

--- Symfony-MAMP/web/config/bundles.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-];
